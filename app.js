784e9fc1940b3dab3643652737f62936ff022d86
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-03-06 11:14:33 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-   
-sayHi(names.john)
+const {readFile, writeFile} = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if(err) {
+    console.log(err)
+    return
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if(err) {
+      console.log(err)
+      return
+    } 
+    const second = result
+    writeFile(
+      './content/result-sync.txt',
+      `here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if(err) {
+          console.log(err)
+          return
+        }
+        console.log(result)
+      }
+    )
+  })
+  
+})
