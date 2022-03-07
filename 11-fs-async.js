063f6ff90ff4f597e367e2deefd4afe41ecb6c09
Michihiro Goto <Michihiro.Goto@msg.group>
2022-03-07 16:40:04 +0100

    update

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,13 +1,28 @@
-const {readFileSync, writeFileSync} = require('fs')
+const {readFile, writeFile} = require('fs')
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
-
-// console.log(first, second)
-
-writeFileSync(
-  './content/result-sync.txt',
-  `here is the result : ${first}, ${second}`,
-  //flag to overwrite or add result
-  {flag : 'a'}
-)
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if(err) {
+    console.log(err)
+    return
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if(err) {
+      console.log(err)
+      return
+    } 
+    const second = result
+    writeFile(
+      './content/result-sync.txt',
+      `here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if(err) {
+          console.log(err)
+          return
+        }
+        console.log(result)
+      }
+    )
+  })
+  
+})
--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-const {readFile, writeFile} = require('fs')
+const http = require('http')
 
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if(err) {
-    console.log(err)
-    return
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Welcome to our home page')  
+  } 
+  if (req.url === '/about') {
+    res.end('Here is our short history')  
   }
-  const first = result
-  readFile('./content/second.txt', 'utf8', (err, result) => {
-    if(err) {
-      console.log(err)
-      return
-    } 
-    const second = result
-    writeFile(
-      './content/result-sync.txt',
-      `here is the result : ${first}, ${second}`,
-      (err, result) => {
-        if(err) {
-          console.log(err)
-          return
-        }
-        console.log(result)
-      }
-    )
-  })
-  
+  // res.end(`
+  // <h1>Oops1</h1>
+  // <p>something went wrong</p>
+  // <a href="/">back home</a>
+  // `)
 })
+
+server.listen(5000)
