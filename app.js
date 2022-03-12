2e6e2bcd2e79941382be124ebbe7b6689a72c41f
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-03-12 18:54:21 +0100

    update

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const http = require('http')
+const _ = require('lodash')
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to our home page')  
-  } 
-  if (req.url === '/about') {
-    res.end('Here is our short history')  
-  }
-  // res.end(`
-  // <h1>Oops1</h1>
-  // <p>something went wrong</p>
-  // <a href="/">back home</a>
-  // `)
-})
-
-server.listen(5000)
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems)
